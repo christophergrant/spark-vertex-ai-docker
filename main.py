a7d89f4a5d2e826dc0c2e5bf8ea7e711dcdadcb9
chris.grant <chris.grant@databricks.com>
2023-01-20 13:30:15 -0800

    add basic logging

--- main.py
+++ main.py
@@ -4,12 +4,15 @@ from pyspark.ml import PipelineModel
 from pyspark.sql import SparkSession
 import pandas as pd
 import numpy
+import logging
+import datetime
 
+logging.basicConfig(level=logging.INFO)
 
 
 class VertexAIPySparkModel:
     def __init__(self, model_path):
-
+        pass
     def predict(self, pandas_df):
         """
         Note: input / output are both pandas dataframe.
@@ -42,18 +45,23 @@ def is_alive():
 # Predict route
 @app.route("/predict", methods=["POST"])
 def predict():
-	print("/predict request")
-	req_json = request.get_json()
+    print("/predict request")
+    logging.info("REQUEST RECEIVED")
+    now = datetime.datetime.now().isoformat()
+    print(f"REQUEST RECEIVED {now}")
+    req_json = request.get_json()
+    
+    
+    pandas_df = pd.DataFrame(req_json)
+    
+    predict_df = spark_model.predict(pandas_df)
 
-	#print(type(req_json))
-
-	pandas_df = pd.DataFrame(req_json)
-
-	spark_model = VertexAIPySparkModel(model_path)
-
-	predict_df = spark_model.predict(pandas_df)
-
-	return jsonify(predict_df.toJSON().first())
+    result = predict_df.toJSON().first()
+    
+    logging.info("INFERENCE COMPLETE")
+    now = datetime.datetime.now().isoformat()
+    print(f"INFERENCE COMPLETE {now}")
+    return jsonify(result)
 
 if __name__ == "__main__":
     app.run(debug=True, host="0.0.0.0", port=8080)
